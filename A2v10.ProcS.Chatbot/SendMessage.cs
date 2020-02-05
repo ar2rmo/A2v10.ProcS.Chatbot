58e1e4b8cc5fff9ed53bacbc296eccfd0089ea8c
artur.moshkola <artur.moshkola@aska.ua>
2020-02-05 16:43:02 +0200

    init

---
+++ A2v10.ProcS.Chatbot/SendMessage.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Threading.Tasks;
+using A2v10.ProcS.Interfaces;
+using BotCore;
+
+namespace A2v10.ProcS.Chatbot
+{
+	public class SendMessage : IWorkflowAction
+	{
+		OutgoingMessage Message { get; set; }
+
+		public Task<ActionResult> Execute(IExecuteContext context)
+		{
+			var mess = new SendMessageMessage(Message);
+			context.SendMessage(mess);
+			return Task.FromResult(ActionResult.Success);
+		}
+	}
+
+	public class SendMessageMessage : MessageBase<Guid>
+	{
+		OutgoingMessage Message { get; set; }
+
+		public SendMessageMessage(OutgoingMessage message) : base(Guid.NewGuid())
+		{
+			Message = message;
+		}
+	}
+}
---
+++ A2v10.ProcS.Chatbot/SendMessageSaga.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Threading.Tasks;
+using A2v10.ProcS.Interfaces;
+using BotCore;
+
+namespace A2v10.ProcS.Chatbot
+{
+	public class SendMessageSage
+	{
+		
+	}
+}
